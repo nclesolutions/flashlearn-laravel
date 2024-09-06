c8dfaa892e4e2b68b8d7dc14305c11648d168001
Stijn <stijn@nclesolutions.com>
2024-09-06 17:25:48 +0200

    Improved SEO #2

--- resources/views/dashboard/flashcards/result.blade.php
+++ resources/views/dashboard/flashcards/result.blade.php
@@ -4,21 +4,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <link rel="icon" href="{{ asset('auth/img/favicon.png') }}" type="image/png" sizes="16x16">
-    <title>FlashLearn - De Complete Digitale Leeromgeving voor Scholen en Individuele Gebruikers</title>
-    <meta name="description" content="FlashLearn is de ultieme digitale leeromgeving voor scholen en individuele gebruikers. Beheer cijfers, huiswerk, en meer met gebruiksvriendelijke tools, terwijl gamification leren leuker maakt. Probeer het nu!">
-    <meta name="keywords" content="FlashLearn, digitale leeromgeving, cijfers beheren, huiswerk systeem, gamified leren, online leren, educatie software, leren leuk maken, scholieren, onderwijs, scholen, individuele gebruikers, leren gamification, digitale educatie">
-    <meta name="robots" content="index, follow">
-    <meta property="og:title" content="FlashLearn - De Complete Digitale Leeromgeving voor Scholen en Individuele Gebruikers">
-    <meta property="og:description" content="Met FlashLearn krijgen scholen en individuele gebruikers toegang tot een veelzijdige digitale leeromgeving die leren efficiënter en leuker maakt. Ontdek de mogelijkheden van deze innovatieve tool.">
-    <meta property="og:url" content="https://www.flashlearn.com">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="{{ asset('auth/img/favicon.png') }}">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="FlashLearn - De Complete Digitale Leeromgeving voor Scholen en Individuele Gebruikers">
-    <meta name="twitter:description" content="FlashLearn biedt alles wat je nodig hebt voor een effectieve en leuke leerervaring, van cijfers en huiswerk tot gamified leren.">
-    <meta name="twitter:image" content="{{ asset('auth/img/favicon.png') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    @include('includes.meta')
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
